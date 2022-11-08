060451f23073d7a0c90061f15cddd1c32eecd628
manuelgaleone <galeonemanuel14@gmail.com>
2022-11-08 14:27:18 +0100

    Add generation function

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,20 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        
+        emailList: [],
       }
+    },
+    methods : {
+
+        mailGen() {
+            
+            for (let i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then(response => {
+                        this.emailList.push(response.data)
+                    })
+            }
+        }   
+
     }
   }).mount('#app')
